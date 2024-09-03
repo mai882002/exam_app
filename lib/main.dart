d2376db0fe753be8967df034d76e742fab17e320
mai882002 <mai466905@gmail.com>
2024-09-04 00:40:28 +0300

    4 commit

--- lib/main.dart
+++ lib/main.dart
@@ -38,6 +38,10 @@ class ExamPage extends StatefulWidget {
 
 class _ExamPageState extends State<ExamPage> {
   List<Padding> answerResult = [];
+  int questionNumber = 0;
+  int correctAnswersCount = 0;
+  bool quizFinished = false;
+
   List<String> questions = [
     'كم عدد سور القرآن الكريم؟',
     'ما هي أوَّل سورةٍ قرآنيَّةٍ نزلت على رسول الله -صلّى الله عليه وسلّم-؟',
@@ -48,20 +52,28 @@ class _ExamPageState extends State<ExamPage> {
     'كم هي المدَّة التي استغرقها نزول القرآن الكريم؟',
     'أين اختبأ رسول الله -عليه السّلام- هو وأبو بكر في طريقه للهجرة؟'
   ];
-  int questionNumber = 0;
-
   List<List<String>> answers = [
     ['120', '124', '121', '125'],
     ['سورة العلق', 'سورة البقرة', 'سورة الكهف', 'سورة الاخلاص'],
     ['سورة الطلاق', 'سورة الفاتحة', 'سورة النمل', 'سورة الحجر'],
     ['سورة الإسراء', 'سورة مريم', 'سورة ال عمران', 'سورة التوبة'],
     ['سورة الرعد', 'سورة يونس', 'سورة يوسف', 'سورة هود'],
-    ['193', ' 139', '194 ', ' 149'],
-    ['20 ', '21', '22 ', '23'],
+    ['193', '139', '194', '149'],
+    ['20', '21', '22', '23'],
     ['غار جبل أحد', 'غار سلع', 'غار ثور', 'غار حراء'],
   ];
 
-  // قائمة الصور
+  List<String> correctanswers = [
+    '124',
+    'سورة العلق',
+    'سورة الفاتحة',
+    'سورة التوبة',
+    'سورة هود',
+    '139',
+    '23',
+    'غار ثور'
+  ];
+
   List<String> images = [
     'images/pic1.jpg',
     'images/pic2.jpg',
@@ -73,139 +85,158 @@ class _ExamPageState extends State<ExamPage> {
     'images/pic8.jpg',
   ];
 
+  void _checkAnswer(String selectedAnswer) {
+    bool isCorrect = selectedAnswer == correctanswers[questionNumber];
+    if (isCorrect) {
+      correctAnswersCount++;
+    }
+
+    setState(() {
+      answerResult.add(
+        Padding(
+          padding: const EdgeInsets.all(3),
+          child: Icon(
+            isCorrect ? Icons.thumb_up : Icons.thumb_down,
+            color: isCorrect ? Colors.green : Colors.red,
+          ),
+        ),
+      );
+
+      questionNumber++;
+      if (questionNumber >= questions.length) {
+        quizFinished = true;
+        _showResultDialog();
+      }
+    });
+  }
+
+  void _showResultDialog() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => Dialog(
+        child: Stack(
+          children: [
+            Positioned.fill(
+              child: Image.asset(
+                'images/images.png', // صورة الخلفية
+                fit: BoxFit.cover,
+              ),
+            ),
+            Positioned.fill(
+              child: Center(
+                child: Container(
+                  color: Colors.black
+                      .withOpacity(0.5), // شبه شفاف لزيادة وضوح النص
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        'لقد أجبت على $correctAnswersCount من أصل ${questions.length} أسئلة بشكل صحيح.',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontFamily: 'Amiri',
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      TextButton(
+                        child: Text(
+                          'إعادة المحاولة',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontFamily: 'Amiri',
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                          setState(() {
+                            questionNumber = 0;
+                            correctAnswersCount = 0;
+                            answerResult.clear();
+                            quizFinished = false;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Row(
-          children: answerResult,
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: answerResult,
+                ),
+              ),
+            ),
+          ],
         ),
         Expanded(
           flex: 5,
-          child: Column(
-            children: [
-              Image.asset(
-                  images[questionNumber]), // عرض الصورة بناءً على السؤال
-              SizedBox(
-                height: 12,
-              ),
-              Text(
-                questions[questionNumber],
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontFamily: 'Amiri',
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
+          child: quizFinished
+              ? Container() // إذا انتهت الأسئلة، لا نحاول عرض أي شيء جديد
+              : Column(
+                  children: [
+                    Image.asset(
+                        images[questionNumber]), // عرض الصورة بناءً على السؤال
+                    SizedBox(height: 12),
+                    Text(
+                      questions[questionNumber], // عرض السؤال الحالي
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontFamily: 'Amiri',
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+        ),
+        if (!quizFinished) // فقط إذا لم ينتهي الاختبار نعرض الأزرار
+          ...answers[questionNumber].map((answer) {
+            return Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: TextButton(
+                  onPressed: () {
+                    _checkAnswer(answer);
+                  },
+                  child: Text(
+                    answer,
+                    style: TextStyle(
+                      color: Color(0xFF212121),
+                      fontSize: 20,
+                      fontFamily: 'Amiri',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  style: TextButton.styleFrom(
+                    backgroundColor: Color(0xFF5d4037),
+                  ),
                 ),
               ),
-            ],
-          ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10),
-            child: TextButton(
-              onPressed: () {
-                setState(() {
-                  questionNumber++;
-                  if (questionNumber >= questions.length) {
-                    questionNumber = 0; // إعادة البدء بعد الانتهاء
-                  }
-                });
-              },
-              child: Text(
-                answers[questionNumber][0],
-                style: TextStyle(
-                  color: Color(0xFF212121),
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              style: TextButton.styleFrom(
-                backgroundColor: Color(0xFF5d4037),
-              ),
-            ),
-          ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10),
-            child: TextButton(
-              onPressed: () {
-                setState(() {
-                  questionNumber++;
-                  if (questionNumber >= questions.length) {
-                    questionNumber = 0;
-                  }
-                });
-              },
-              child: Text(
-                answers[questionNumber][1],
-                style: TextStyle(
-                  color: Color(0xFF212121),
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              style: TextButton.styleFrom(
-                backgroundColor: Color(0xFF5d4037),
-              ),
-            ),
-          ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10),
-            child: TextButton(
-              onPressed: () {
-                setState(() {
-                  questionNumber++;
-                  if (questionNumber >= questions.length) {
-                    questionNumber = 0;
-                  }
-                });
-              },
-              child: Text(
-                answers[questionNumber][2],
-                style: TextStyle(
-                  color: Color(0xFF212121),
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              style: TextButton.styleFrom(
-                backgroundColor: Color(0xFF5d4037),
-              ),
-            ),
-          ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10),
-            child: TextButton(
-              onPressed: () {
-                setState(() {
-                  questionNumber++;
-                  if (questionNumber >= questions.length) {
-                    questionNumber = 0;
-                  }
-                });
-              },
-              child: Text(
-                answers[questionNumber][3],
-                style: TextStyle(
-                  color: Color(0xFF212121),
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              style: TextButton.styleFrom(
-                backgroundColor: Color(0xFF5d4037),
-              ),
-            ),
-          ),
-        ),
+            );
+          }).toList(),
       ],
     );
   }
